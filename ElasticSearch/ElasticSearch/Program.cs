55a7dbb6f130efc8a9c5c4a7cd337b086e980320
Amit Prakash <aprakash@internal.tavisca.com>
2017-09-25 19:14:52 +0530

    some changes

--- ElasticSearch/ElasticSearch/Program.cs
+++ ElasticSearch/ElasticSearch/Program.cs
@@ -7,26 +7,92 @@ using Nest;
 
 namespace ElasticSearch
 {
+    
     public class Program
     {
         public static Uri node;
         public static ConnectionSettings settings;
         public static ElasticClient client;
 
+
         static void Main(string[] args)
         {
             node = new Uri("http://172.16.14.68:9200/");
             var settings = new ConnectionSettings(node);
             client = new ElasticClient(settings);
-            Employee employee = new Employee();
 
-            employee.Id = "A101";
-            employee.FName = "Amit";
-            employee.MName = "NA";
-            employee.LNAme = "Prakash";
-            employee.Address = "Pune";
+            Console.WriteLine("Enter the operation you Want to perform 1)Insert 2)Delete 3)Search");
+            var choice = Console.ReadLine();
+
+            bool result = Int32.TryParse(choice, out int ch);
+
+            {
+                while (true)
+                {
+                    switch (ch)
+                    {
+
+                        case 1:
+                            Insert();
+                            break;
+
+                        case 2:
+
+                            DeleteOperation();
+                            break;
+
+                        case 3:
+                            SearchOperation();
+                            break;
+                    }
+                }
+            }
+        }
+        public static void Insert()
+        {
+            Employee employee = new Employee();
+            Console.WriteLine("Enter Id");
+            employee.Id = Console.ReadLine();
+            Console.WriteLine("Enter First Name");
+            employee.FName = Console.ReadLine();
+            Console.WriteLine("Enter Middle Name");
+            employee.MName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            employee.LNAme =Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            employee.Address = Console.ReadLine();
             AddIndex(employee);
-            List<Employee> listOfEmployee = Search("A101");
+           
+        }
+
+
+        public static void DeleteOperation()
+        {
+            Console.WriteLine("Enter the Id you want to delete");
+            var result = Console.ReadLine();
+            Delete(result);
+
+        }
+
+        public static bool Delete(string word)
+        {
+            bool status;
+
+            var response = client.Delete<Employee>(word, d => d
+                .Index("employee")
+                .Type("mydata"));
+
+            if (response.IsValid)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            return status;
+
         }
 
         public static void AddIndex(Employee employee)
@@ -35,11 +101,19 @@ namespace ElasticSearch
             {
                 Id = employee.Id,
                 FName = employee.FName,
-                MName=employee.MName,
-                LNAme=employee.LNAme,
-                Address=employee.Address,
+                MName = employee.MName,
+                LNAme = employee.LNAme,
+                Address = employee.Address,
             };
             client.Index<Employee>(jsonData, x => x.Index("employee").Type("mydata").Id(employee.Id).Refresh(Elasticsearch.Net.Refresh.True));
+
+        }
+
+        public static void SearchOperation()
+        {
+            Console.WriteLine("Enter the Id you want to search");
+            var result = Console.ReadLine();
+            List<Employee> listOfEmployee = Search(result);
 
         }
 
